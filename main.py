aaac3641e3be75c71bdc8fbb80e1808b15b4802f
khaidSmartOSC <khaid@smartosc.com>
2023-08-23 17:50:15 +0700

    Finish part: Path Parameters, Query Parameters, Request Body, Query Parameters and String Valdation

---
+++ main.py
@@ -0,0 +1,116 @@
+from fastapi import FastAPI, Query
+from enum import Enum
+from pydantic import BaseModel
+from typing import Annotated
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"dit con me may"}
+
+
+@app.post("/")
+async def post():
+    return {"message": "hello from post route"}
+
+
+@app.put("/")
+async def post():
+    return {"message": "hello from put route"}
+
+
+@app.get("/users")
+async def list_users(item_id: int):
+    return {"message": "list users route"}
+
+
+@app.get("/users/{user_id}")
+async def get_items(user_id: int):
+    return {"user_id": user_id}
+
+
+class FoodEnum(str, Enum):
+    fruits = "fruits"
+    vegetables = "vegetables"
+    dairy = "dairy"
+
+
+@app.get("/foods/{food_name}")
+async def get_food(food_name: FoodEnum):
+    if food_name == FoodEnum.vegetables:
+        return {"food_name": food_name, "message": "you are healthy"}
+    if food_name.value == "fruits":
+        return {
+            "food_name": food_name,
+            "message": "you are still healthy, but like sweet things",
+        }
+    return {"food_name": food_name, "message": "i like chocolate milk"}
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items")
+async def list_items(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
+@app.get("/items/{item_id}")
+async def get_items(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "ditconmemay"})
+    return item
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def get_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "ditconmemay"})
+        return item
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@app.post("/items")
+async def create_items(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+@app.put("/items/{item_id}")
+async def create_item_with_put(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
+
+
+@app.get("/items/")
+async def read_items(
+    q: str
+    | None = Query(
+        None, min_length=1, max_length=10, description="dcmm", title="ddjcdsklfjakl"
+    )
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
