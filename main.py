4d15c927333406deffdfe4bc2e3f5a7d3c043753
khaidSmartOSC <khaid@smartosc.com>
2023-08-30 15:42:09 +0700

    Finish Part: Body - Nested Models

--- main.py
+++ main.py
@@ -1,82 +1,211 @@
-from fastapi import FastAPI, Query, Path
+from fastapi import FastAPI, Query, Path, Body
 from enum import Enum
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, HttpUrl
 from typing import Annotated
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"dit con me may"}
+# @app.get("/")
+# async def root():
+#     return {"dit con me may"}
 
 
-@app.post("/")
-async def post():
-    return {"message": "hello from post route"}
+# @app.post("/")
+# async def post():
+#     return {"message": "hello from post route"}
 
 
-@app.put("/")
-async def post():
-    return {"message": "hello from put route"}
+# @app.put("/")
+# async def post():
+#     return {"message": "hello from put route"}
 
 
-@app.get("/users")
-async def list_users(item_id: int):
-    return {"message": "list users route"}
+# @app.get("/users")
+# async def list_users(item_id: int):
+#     return {"message": "list users route"}
 
 
-@app.get("/users/{user_id}")
-async def get_items(user_id: int):
-    return {"user_id": user_id}
+# @app.get("/users/{user_id}")
+# async def get_items(user_id: int):
+#     return {"user_id": user_id}
 
 
-class FoodEnum(str, Enum):
-    fruits = "fruits"
-    vegetables = "vegetables"
-    dairy = "dairy"
+# class FoodEnum(str, Enum):
+#     fruits = "fruits"
+#     vegetables = "vegetables"
+#     dairy = "dairy"
 
 
-@app.get("/foods/{food_name}")
-async def get_food(food_name: FoodEnum):
-    if food_name == FoodEnum.vegetables:
-        return {"food_name": food_name, "message": "you are healthy"}
-    if food_name.value == "fruits":
-        return {
-            "food_name": food_name,
-            "message": "you are still healthy, but like sweet things",
-        }
-    return {"food_name": food_name, "message": "i like chocolate milk"}
+# @app.get("/foods/{food_name}")
+# async def get_food(food_name: FoodEnum):
+#     if food_name == FoodEnum.vegetables:
+#         return {"food_name": food_name, "message": "you are healthy"}
+#     if food_name.value == "fruits":
+#         return {
+#             "food_name": food_name,
+#             "message": "you are still healthy, but like sweet things",
+#         }
+#     return {"food_name": food_name, "message": "i like chocolate milk"}
 
 
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+# fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
-@app.get("/items")
-async def list_items(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]
+# @app.get("/items")
+# async def list_items(skip: int = 0, limit: int = 10):
+#     return fake_items_db[skip : skip + limit]
 
 
-@app.get("/items/{item_id}")
-async def get_items(item_id: str, q: str | None = None, short: bool = False):
-    item = {"item_id": item_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update({"description": "ditconmemay"})
-    return item
+# @app.get("/items/{item_id}")
+# async def get_items(item_id: str, q: str | None = None, short: bool = False):
+#     item = {"item_id": item_id}
+#     if q:
+#         item.update({"q": q})
+#     if not short:
+#         item.update({"description": "ditconmemay"})
+#     return item
 
 
-@app.get("/users/{user_id}/items/{item_id}")
-async def get_user_item(
-    user_id: int, item_id: str, q: str | None = None, short: bool = False
-):
-    item = {"item_id": item_id, "owner_id": user_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update({"description": "ditconmemay"})
-        return item
+# @app.get("/users/{user_id}/items/{item_id}")
+# async def get_user_item(
+#     user_id: int, item_id: str, q: str | None = None, short: bool = False
+# ):
+#     item = {"item_id": item_id, "owner_id": user_id}
+#     if q:
+#         item.update({"q": q})
+#     if not short:
+#         item.update({"description": "ditconmemay"})
+#         return item
+
+
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float | None = None
+
+
+# @app.post("/items")
+# async def create_items(item: Item):
+#     item_dict = item.dict()
+#     if item.tax:
+#         price_with_tax = item.price + item.tax
+#         item_dict.update({"price_with_tax": price_with_tax})
+#     return item_dict
+
+
+# @app.put("/items/{item_id}")
+# async def create_item_with_put(item_id: int, item: Item, q: str | None = None):
+#     result = {"item_id": item_id, **item.dict()}
+#     if q:
+#         result.update({"q": q})
+#     return result
+
+
+# @app.get("/items")
+# async def read_items(
+#     q: str
+#     | None = Query(
+#         None,
+#         min_length=3,
+#         max_length=10,
+#         title="Sample query string",
+#         description="This is a sample query string.",
+#         alias="item-query",
+#     )
+# ):
+#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+
+# @app.get("/items_hidden")
+# async def hidden_query_route(
+#     hidden_query: str | None = Query(None, include_in_schema=False)
+# ):
+#     if hidden_query:
+#         return {"hidden_query": hidden_query}
+#     return {"hidden_query": "Not found"}
+
+
+# @app.get("/items_validation/item_id")
+# async def read_items_validation(
+#     *,
+#     item_id: int = Path(..., title="The ID of the item to get", ge=10, le=100),
+#     q: str,
+#     size: float = Query(..., gt=0, lt=7.75)
+# ):
+#     results = {"item_id": item_id, "size": size}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+"""
+Part: Body - Multiple Parameters
+"""
+
+
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float | None = None
+
+
+# class User(BaseModel):
+#     username: str
+#     full_name: str | None = None
+
+
+# @app.put("/items/{item_id}")
+# async def update_items(
+#     *,
+#     item_id: int = Path(..., title="The ID of the item to get", ge=0, le=150),
+#     q: str | None = None,
+#     # item: Item | None = None,
+#     item: Item = Body(..., embed=True),
+#     user: User,
+#     importance: int = Body(...)
+# ):
+#     results = {"item_id": item_id}
+#     if q:
+#         results.update({"q": q})
+#     if item:
+#         results.update({"item": item})
+#     if user:
+#         results.update({"user": user})
+#     if importance:
+#         results.update({"importance": importance})
+#     return results
+
+"""
+Part: Body - Fields
+"""
+
+
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = Field(
+#         None, title="The description of the item", max_length=300
+#     )
+#     price: float = Field(..., gt=0, description="The price must be greater than zero")
+#     tax: float | None = None
+
+
+# @app.put("items/{item_id}")
+# async def update_item(item_id: int, item: Item = Body(...)):
+#     results = {"item_id": item_id, "item": item}
+
+"""
+Part: Body - Nested Models
+"""
+
+
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
 
 
 class Item(BaseModel):
@@ -84,60 +213,34 @@ class Item(BaseModel):
     description: str | None = None
     price: float
     tax: float | None = None
+    # tags: list[str] = []
+    tags: set[str] = set()
+    image: list[Image] | None = None
 
 
-@app.post("/items")
-async def create_items(item: Item):
-    item_dict = item.dict()
-    if item.tax:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-    return item_dict
+class Offer(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    items: list[Item]
 
 
 @app.put("/items/{item_id}")
-async def create_item_with_put(item_id: int, item: Item, q: str | None = None):
-    result = {"item_id": item_id, **item.dict()}
-    if q:
-        result.update({"q": q})
-    return result
-
-
-@app.get("/items")
-async def read_items(
-    q: str
-    | None = Query(
-        None,
-        min_length=3,
-        max_length=10,
-        title="Sample query string",
-        description="This is a sample query string.",
-        alias="item-query",
-    )
-):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q": q})
+async def update_item(item_id: int, item: Item):
+    results = {"item_id": item_id, "item": item}
     return results
 
 
-@app.get("/items_hidden")
-async def hidden_query_route(
-    hidden_query: str | None = Query(None, include_in_schema=False)
-):
-    if hidden_query:
-        return {"hidden_query": hidden_query}
-    return {"hidden_query": "Not found"}
+@app.post("/offers")
+async def create_offer(offer: Offer = Body(..., embed=True)):
+    return offer
 
 
-@app.get("/items_validation/item_id")
-async def read_items_validation(
-    *,
-    item_id: int = Path(..., title="The ID of the item to get", ge=10, le=100),
-    q: str,
-    size: float = Query(..., gt=0, lt=7.75)
-):
-    results = {"item_id": item_id, "size": size}
-    if q:
-        results.update({"q": q})
-    return results
+@app.post("/images/multiple")
+async def create_multiple_images(images: list[Image]):
+    return images
+
+
+@app.post("/blah")
+async def create_some_blahs(blahs: dict[int, float]):
+    return blahs
