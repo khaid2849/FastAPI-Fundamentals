090d30aab561f80f668042cd2cf8dfafebc4be2b
khaidSmartOSC <khaid@smartosc.com>
2023-09-05 15:56:00 +0700

    Finish till part: Handling Errors

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-from fastapi import FastAPI, Query, Path, Body
+from fastapi import (
+    FastAPI,
+    HTTPException,
+    Query,
+    Path,
+    Body,
+    Cookie,
+    Header,
+    Request,
+    status,
+    Form,
+    File,
+    UploadFile,
+)
 from enum import Enum
-from pydantic import BaseModel, Field, HttpUrl
-from typing import Annotated
+from fastapi.encoders import jsonable_encoder
+from fastapi.exception_handlers import (
+    http_exception_handler,
+    request_validation_exception_handler,
+)
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse, PlainTextResponse
+from pydantic import BaseModel, Field, HttpUrl, EmailStr
+from typing import Annotated, Literal
+from uuid import UUID
+from datetime import datetime, time, timedelta
+from starlette.responses import HTMLResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 app = FastAPI()
 
@@ -202,45 +226,462 @@ Part: Body - Fields
 Part: Body - Nested Models
 """
 
-
-class Image(BaseModel):
-    url: HttpUrl
-    name: str
-
-
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-    # tags: list[str] = []
-    tags: set[str] = set()
-    image: list[Image] | None = None
+# class Image(BaseModel):
+#     url: HttpUrl
+#     name: str
 
 
-class Offer(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    items: list[Item]
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float | None = None
+#     # tags: list[str] = []
+#     tags: set[str] = set()
+#     image: list[Image] | None = None
 
 
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
-    results = {"item_id": item_id, "item": item}
-    return results
+# class Offer(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     items: list[Item]
 
 
-@app.post("/offers")
-async def create_offer(offer: Offer = Body(..., embed=True)):
-    return offer
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item):
+#     results = {"item_id": item_id, "item": item}
+#     return results
 
 
-@app.post("/images/multiple")
-async def create_multiple_images(images: list[Image]):
-    return images
+# @app.post("/offers")
+# async def create_offer(offer: Offer = Body(..., embed=True)):
+#     return offer
 
 
-@app.post("/blah")
-async def create_some_blahs(blahs: dict[int, float]):
-    return blahs
+# @app.post("/images/multiple")
+# async def create_multiple_images(images: list[Image]):
+#     return images
+
+
+# @app.post("/blah")
+# async def create_some_blahs(blahs: dict[int, float]):
+#     return blahs
+
+"""
+Part: Decalre Request Example Data
+"""
+
+
+# class Item(BaseModel):
+#     name: str = Field(..., examples=["Foo"])
+#     description: str | None = Field(None, examples=["dit mem may"])
+#     price: float = Field(..., examples=[10])
+#     tax: float | None = Field(None, examples=[0.5])
+
+# model_config = {
+#     "json_schema_extra": {
+#         "examples": [
+#             {
+#                 "name": "Foo",
+#                 "description": "A very nice Item",
+#                 "price": 35.4,
+#                 "tax": 3.2,
+#             }
+#         ]
+#     }
+# }
+
+
+# @app.put("/items/{item_id}")
+# async def update_items(item_id: int, item: Item):
+#     results = {"item_id": item_id, "item": item}
+#     return results
+
+
+# @app.put("/items/{item_id}")
+# async def update_item(
+#     item_id: int,
+#     item: Annotated[
+#         Item,
+#         Body(
+#             examples=[
+#                 {
+#                     "name": "Foo",
+#                     "description": "A very nice Item",
+#                     "price": 35.4,
+#                     "tax": 3.2,
+#                 }
+#             ],
+#         ),
+#     ],
+# ):
+#     results = {"item_id": item_id, "item": item}
+#     return results
+
+"""
+Part: Extra Data Types
+"""
+
+
+# async def read_items(
+#     item_id: UUID,
+#     start_datetime: Annotated[datetime | None, Body()] = None,
+#     end_datetime: Annotated[datetime | None, Body()] = None,
+#     repeat_at: Annotated[time | None, Body()] = None,
+#     process_after: Annotated[timedelta | None, Body()] = None,
+# ):
+#     start_process = start_datetime + process_after
+#     duration = end_datetime - start_process
+#     return {
+#         "item_id": item_id,
+#         "start_datetime": start_datetime,
+#         "end_datetime": end_datetime,
+#         "repeat_at": repeat_at,
+#         "process_after": process_after,
+#         "start_process": start_process,
+#         "duration": duration,
+#     }
+
+
+"""
+Part: Cookie and Header Parameters
+"""
+
+# @app.get("/items")
+# async def read_items(
+#     cookie_id: str | None = Cookie(None),
+#     accept_encoding: str | None = Header(None),
+#     sec_ch_ua: str | None = Header(None),
+#     user_agent: str | None = Header(None),
+#     x_token: list[str] | None = Header(None),
+# ):
+#     return {
+#         "cookie_id": cookie_id,
+#         "Accept-Encoding": accept_encoding,
+#         "sec-ch-ua": sec_ch_ua,
+#         "User-Agent": user_agent,
+#         "X-Token values": x_token,
+#     }
+
+
+# @app.get("/items/")
+# async def read_items(q: Annotated[list[str] | None, Query()] = None):
+#     query_items = {"q": q}
+#     return query_items
+
+"""
+Part: Response Model
+"""
+
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float = 10.5
+#     tags: list[str] = []
+
+
+# items = {
+#     "foo": {"name": "Foo", "price": 50.2},
+#     "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+#     "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+# }
+
+
+# @app.get("/items/{item_id}", response_model=Item, response_model_exclude_unset=True)
+# async def read_item(item_id: Literal["foo", "bar", "baz"]):
+#     return items[item_id]
+
+
+# @app.post("/items/", response_model=Item)
+# async def create_item(item: Item):
+#     return item
+
+
+# class UserBase(BaseModel):
+#     username: str
+#     email: EmailStr
+#     full_name: str | None = None
+
+
+# class UserIn(UserBase):
+#     password: str
+
+
+# class UserOut(UserBase):
+#     pass
+
+
+# @app.post("/user/", response_model=UserOut)
+# async def create_user(user: UserIn):
+#     return user
+
+
+# @app.get(
+#     "/items/{item_id}/name",
+#     response_model=Item,
+#     response_model_include={"name", "description"},
+# )
+# async def read_item_name(item_id: Literal["foo", "bar", "baz"]):
+#     return items[item_id]
+
+
+# @app.get("/items/{item_id}/public", response_model=Item, response_model_exclude={"tax"})
+# async def read_items_public_data(item_id: Literal["foo", "bar", "baz"]):
+#     return items[item_id]
+
+"""
+Part: Extra model
+"""
+
+
+# class UserBase(BaseModel):
+#     username: str
+#     email: EmailStr
+#     full_name: str | None = None
+
+
+# class UserIn(UserBase):
+#     password: str
+
+
+# class UserOut(UserBase):
+#     pass
+
+
+# class UserInDB(UserBase):
+#     hashed_password: str
+
+
+# def fake_password_hashed(raw_password: str):
+#     return f"supersecret{raw_password}"
+
+
+# def fake_save_user(user_in: UserIn):
+#     hashed_password = fake_password_hashed(user_in.password)
+#     user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
+
+#     print("User 'saved'.")
+#     return user_in_db
+
+
+# @app.post("/user", response_model=UserOut)
+# async def create_user(user_in: UserIn):
+#     user_saved = fake_save_user(user_in)
+#     return user_saved
+
+
+# class BaseItem(BaseModel):
+#     description: str
+#     type: str
+
+
+# class CarItem(BaseItem):
+#     type: str = "car"
+
+
+# class PlaneItem(BaseItem):
+#     type: str = "plane"
+#     size: int
+
+
+# items = {
+#     "item1": {"description": "All my friends drive a low rider", "type": "car"},
+#     "item2": {
+#         "description": "Music is my aeroplane, it's my aeroplane",
+#         "type": "plane",
+#         "size": 5,
+#     },
+# }
+
+
+# @app.get("/items/{item_id}", response_model=PlaneItem | CarItem)
+# async def read_item(item_id: Literal["item1", "item2"]):
+#     return items[item_id]
+
+
+# class ListItem(BaseModel):
+#     name: str
+#     description: str
+
+
+# list_items = [
+#     {"name": "Foo", "description": "There comes my hero"},
+#     {"name": "Red", "description": "It's my aeroplane"},
+# ]
+
+
+# @app.get("/list_items", response_model=list[ListItem])
+# async def read_items():
+#     return items
+
+
+# @app.get("/arbitrary", response_model=dict[str, float])
+# async def get_arbitrary():
+#     return {"foo": 1, "bar": "2"}
+
+"""
+Part: Response Status Code
+"""
+
+
+# @app.post("/items/", status_code=201)
+# async def create_item(name: str):
+#     return {"name": name}
+
+
+# @app.post("/items/", status_code=status.HTTP_201_CREATED)
+# async def create_item(name: str):
+#     return {"name": name}
+
+"""
+Part: Form Fields
+"""
+
+
+# @app.post("/login")
+# async def login(username: str = Form(...), password: str = Form(...)):
+#     return {"username": username}
+
+
+# @app.post("/login-json/")
+# async def login_json(username: str = Body(...), password: str = Body(...)):
+#     return {"username": username}
+
+"""
+Part: Request Files
+"""
+
+
+# @app.post("/files")
+# async def create_file(
+#     files: list[bytes] = File(..., description="Files read as bytes")
+# ):
+#     return {"file_sizes": [len(file) for file in files]}
+
+
+# @app.post("/uploadfile")
+# async def create_upload_file(file: UploadFile):
+#     return {"filename": file.filename}
+
+
+# @app.get("/")
+# async def main():
+#     content = """
+# <body>
+# <form action="/files/" enctype="multipart/form-data" method="post">
+# <input name="files" type="file" multiple>
+# <input type="submit">
+# </form>
+# <form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+# <input name="files" type="file" multiple>
+# <input type="submit">
+# </form>
+# </body>
+#     """
+#     return HTMLResponse(content=content)
+
+"""
+Part: Request Forms and Files
+"""
+
+
+# @app.post("/files/")
+# async def create_file(
+#     file: bytes = File(), fileb: UploadFile = File(...), token: str = Form(...)
+# ):
+#     return {
+#         "file_size": len(file),
+#         "token": token,
+#         "fileb_content_type": fileb.content_type,
+#     }
+
+
+"""
+Part: Handling Errors
+"""
+items = {"foo": "The Foo Wrestlers"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str):
+    if item_id not in items:
+        raise HTTPException(
+            status_code=404,
+            detail="Item not found",
+            headers={"X-Error": "There goes my error"},
+        )
+    return {"item": items[item_id]}
+
+
+class UnicornException(Exception):
+    def __init__(self, name: str):
+        self.name = name
+
+
+@app.exception_handler(UnicornException)
+async def unicorn_exception_handler(request: Request, exc: UnicornException):
+    return JSONResponse(
+        status_code=418,
+        content={"message": f"Oops! {exc.name} did something. There goes a rainbow..."},
+    )
+
+
+@app.get("/unicorns/{name}")
+async def read_unicorns(name: str):
+    if name == "yolo":
+        raise UnicornException(name=name)
+    return {"unicorn_name": name}
+
+
+# @app.exception_handler(RequestValidationError)
+# async def validation_exception_handler(request, exc):
+#     return PlainTextResponse(str(exc), status_code=400)
+
+
+# @app.exception_handler(StarletteHTTPException)
+# async def http_exception_handler(request, exc):
+#     return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
+
+
+# @app.get("/validation_items/item_id")
+# async def read_validation_items(item_id: int):
+#     if item_id == 3:
+#         raise HTTPException(status_code=418, detail="Nope! I don't like 3")
+#     return {"item_id": item_id}
+
+
+# @app.exception_handler(RequestValidationError)
+# async def validation_exception_handler(request: Request, exc: RequestValidationError):
+#     return JSONResponse(
+#         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+#         content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
+#     )
+
+
+# class Item(BaseModel):
+#     title: str
+#     size: int
+
+
+# @app.post("/items/")
+# async def create_item(item: Item):
+#     return item
+
+
+@app.exception_handler(StarletteHTTPException)
+async def custom_http_exception_handler(request, exc):
+    return await http_exception_handler(request, exc)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    return await request_validation_exception_handler(request, exc)
+
+
+@app.get("/blah_items/{item_id}")
+async def read_items(item_id: int):
+    if item_id == 3:
+        raise HTTPException(status_code=418, detail="Nope! I don't like 3.")
+    return {"item_id": item_id}
