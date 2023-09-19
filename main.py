046881b4deb4def9e08910946ce05006729dd264
khaidSmartOSC <khaid@smartosc.com>
2023-09-19 15:39:56 +0700

    Working on Part: Security

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ from uuid import UUID
 from datetime import datetime, time, timedelta
 from starlette.responses import HTMLResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 app = FastAPI()
 
@@ -891,3 +892,93 @@ Part: Dependencies in path operation decorators
 # async def read_users():
 #     return [{"username": "Rick"}, {"username": "Morty"}]
 
+"""
+Part: Part: Security
+"""
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+fake_users_db = {
+    "khaid": dict(
+        username="khaid",
+        full_name="Khai Dang",
+        email="khaid@gmail.com",
+        hashed_password="fakehashedsecret",
+        disabled=False,
+    ),
+    "ngocb": dict(
+        username="ngocb",
+        full_name="Ngoc Bitch",
+        email="khaid@gmail.com",
+        hashed_password="fakehashedsecret",
+        disabled=True,
+    ),
+}
+
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def fake_decode_token(token):
+    return get_user(fake_users_db, token)
+
+
+def fake_hashed_password(password):
+    return f"fakehashed{password}"
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Autheticate": "Bearer"},
+        )
+    return user
+
+
+async def get_current_active_user(
+    current_user: User = Depends(get_current_user),
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.get("/items")
+async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
+    return {"token": token}
+
+
+@app.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user_dict = fake_users_db[form_data.username]
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+
+    hashed_password = fake_hashed_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
+@app.get("/users/me")
+async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]):
+    return current_user
